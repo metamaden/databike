96bcc14a11cdd7f5a5878e2b3c85404d2170c5b4
Sean Maden <metamaden@users.noreply.github.com>
2020-03-29 12:38:30 -0700

    adds app.R

---
+++ R/app.R
@@ -0,0 +1,16 @@
+#!/usr/bin/env R
+
+# Data for app
+# Manage scene sequences for drives
+
+# vars for drive
+# time (cycles in animation)
+# bike wear (change chance of "breakdown")
+# tire wear
+
+# sequence for "normal drive"
+# 1. idle
+# 2. drive
+
+# test
+

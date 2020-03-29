9a2b5ad57e3feb610dea8b9cb4af48b655521302
Sean Maden <metamaden@users.noreply.github.com>
2020-03-28 18:07:36 -0700

    adds grids

--- scripts/autoupdate_graphic.R
+++ scripts/autoupdate_graphic.R
@@ -3,43 +3,84 @@
 # =__%
 # O O
 
+grid.newpage()
+
+sky1 <- paste0(c(rep(" ", 7), "-"))
+sky2 <- paste0(c(rep(" ", 4), "-", rep(" ", )))
+sky3 <- c(rep(" ", 7), "-")
+
+exhaust1 <- "-~"
+exhaust2 <- "~-"
+bikespace <- "  "
+bike1 <- paste0(exhaust1, "=__%", bikespace)
+bike2 <- paste0(exhaust2, "=__%", bikespace)
+
+
+wheels <- "__00_"
+frame1 <- paste0(c(bike, 
+                   wheels), 
+                 collapse = "\n")
+
+grid.text(frame1, 
+          x = 0.5, y = 0.5,
+          gp = gpar(fontsize = 20, 
+                    col = "black")
+          )
+
+
 # notes
 
-space.char <- "_"
+blank.sym <- "_"
+f1.l1 <- rep(blank.sym, 10)
+f1.l2 <- c(blank.sym, "=__%", 
+           rep(blank.sym, 5))
+f1.l3 <- c(blank.sym, "0 0", 
+           rep(blank.sym, 6))
+grid.newpage()
+
+vv <- "____"
+txtv <- paste(c(f1.l1, f1.l2, f1.l3), 
+              sep = "\n")
+grid.text(txtv, x = 0.5, y = 0.5,
+          gp = gpar(fontsize = 20, 
+                    col = "black"))
+
 
 # frame dims: 10 cols, 3 rows
-blank <- c(rep(space.char, 10),
-           rep(space.char, 10),
-           rep(space.char, 10))
 
 # make viewport
-
 vp <- function(){
   plot.new()
   legend("center", 
-         legend = c(blank[1], blank[2], blank[3]))
+         legend = c(blank.sym[1], 
+                    blank.sym[2], 
+                    blank.sym[3]),
+         text.width = 10)
 }
 
-
+#----------------
 # scooter moving
+#----------------
+# frame1 
+f1.l1 <- rep(blank.sym, 10)
+f1.l2 <- paste0(blank.sym, "=__%", rep(blank.sym, 5))
+f1.l3 <- paste0(blank.sym, "0 0", rep(blank.sym, 6))
 
-frame <- function(l1, l2, l3){
-  legend("center", legend = c(l1, l2, l3))
+frame <- function(f1.l1, f1.l2, f1.l3){
+  # prints a single frame
+  # l1 = first line
+  plot.new()
+  legend("center", 
+         legend = c(f1.l1, f1.l2, 
+                    f1.l3),
+         text.width = 10)
 }
 
 vp()
-
 frame(f1.l1, f1.l2, f1.l3)
 
-# frames and lines
-f1.l1 <- rep(" ", 10)
-f1.l2 <- paste0(" ", "=__%", rep(" ", 5))
-f1.l3 <- paste0(" ", "0 0", rep(" ", 6))
-
-
-
 bikemove <- function(frame1, frame2){
-  legend("center",legend = frame1)
+  legend("center", legend = frame1)
 }
 
 # scooter stationary

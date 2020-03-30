81c1440aa32f3bba6677d5303e238e8eeaaea8e7
Sean Maden <metamaden@users.noreply.github.com>
2020-03-30 08:15:21 -0700

    Update app.R

--- R/app.R
+++ R/app.R
@@ -17,6 +17,136 @@
 library(svDialogs)
 library(grid)
 
+#-------------
+# dependencies
+#-------------
+{
+  
+  # ridseq
+  rt <- "short" # short, medium, long
+  ride.type <- rt
+  ride.dur <- ifelse(rt == "short", 30, 
+                     ifelse(rt == "medium", 50, 
+                            ifelse(rt == "long", 100, "NA")))
+  ride.seq <- seq(1, ride.dur, 1)
+  
+  # oseq
+  
+  stationary <- c("_______\n  =__% \n__0 0__")
+  blink <- c("_______\n       \n__   __")
+  fv.idle <- c(stationary, blink)
+  
+  drive1 <- c("     **\n `=__% \n__O o_-")
+  drive2 <- c("   ** *\n`=__%  \n__o O__")
+  drive3 <- c("***     \n `=__% \n_-o o__")
+  drive4 <- c("       \n `=__% \n-_0 0__")
+  fv.drive <- c(drive1, drive2, drive3, drive4)
+  
+  obstacle.uifun <- function(){
+    ui.msg <- dlg_message("cancel ride?", "yesno")$res
+    return(ifelse(ui.msg == "yes", 0, 1))
+  }
+  
+  endride.uifun <- function(msgstr){
+    ui.msg <- dlg_message(msgstr, "ok")
+  }
+  
+  ride.normal <- function(alabel = "ride: normal", msgperc,
+                          framevector = fv.drive, ssint = 0.1, loops = 1){
+    grid.newpage()
+    c = 1
+    while(c <= loops){
+      for(f in framevector){
+        framewithlabel <- paste0(c(alabel, f, msgperc), 
+                                 collapse = "\n")
+        grid.newpage()
+        grid.text(framewithlabel)
+        Sys.sleep(ssint)
+      }
+      c = c + 1
+    }
+  }
+  
+  ride.obstacle <- function(alabel = "ride: obstacle!", msgperc,
+                            framevector1 = fv.drive, 
+                            framevector2 = fv.obstacle, 
+                            ssint = 0.5, loops = 2){
+    grid.newpage()
+    c = 1
+    while(c <= loops){
+      for(i in 1:length(framevector1)){
+        grid.newpage()
+        # print ride animation
+        fs <- framevector1[i]
+        frame1 <- paste0(c(alabel, fs, msgperc), 
+                         collapse = "\n")
+        grid.text(frame1)
+        # print obstacle animation
+        fo <- framevector2[i]
+        frame2 <- paste0(c(" ", fo),
+                         collapse = "\n")
+        grid.text(frame2)
+        Sys.sleep(ssint)
+      }
+      c = c + 1
+    }
+  }
+  
+  ride <- function(ride.seq, o.seq, bc,
+                   tdist, onum, bcond){
+    
+    # global stats
+    # tdist # global mileage
+    # onum # global obstacle count
+    # bcond # bike condition
+    
+    require(grid)
+    # add bike condition stuff
+    grid.newpage()
+    # baseline stats for ride
+    ride.finished <- 0; ride.status <- 1
+    perc.finished <- 0
+    oride <- 0
+    # bcchange <- bc # bike condition
+    while(ride.status > 0){
+      for(c in ride.seq){
+        perc.finished <- 100*(c/length(ride.seq))
+        msgperc <- paste0("ride progress: ", perc.finished, "%")
+        ride.finished <- ifelse(c == max(ride.seq),
+                                1, 0)
+        if(c %in% o.seq){
+          ride.obstacle(msgperc = msgperc)
+          ride.status <- obstacle.uifun()
+        } else{
+          ride.normal(msgperc = msgperc)
+        }
+        if(ride.status == 0){
+          msgstr <- "ride over!"
+          msgstr <- ifelse(ride.finished == 1,
+                           paste0(msgstr, 
+                                  " the ride has ended!! \n",
+                                  "mileage = ", tdist + c,
+                                  ""),
+                           paste0(msgstr,
+                                  " the ride has ended!! \n",
+                                  "mileage = ", tdist + c,
+                                  "obstacles = ", onum + oride)
+          )
+          # get running stats
+          lr <- list("tot.dist",
+                     "tot.obstacles",
+                     "bike.condition")
+          
+          endride.uifun(msgstr)
+          return(NULL)
+        }
+      }
+    }
+    grid.newpage()
+    return(bc)
+  }
+}
+
 #---------
 # main app
 #---------
@@ -34,8 +164,12 @@ rpm <- 1.5 # repair prob modifier to bdi
 # maintain?
 
 rungame <- function(){
+  # garage time
   do_idle()
-  
+  # get rideseq and oseq
+  ride.seq <- get_rideseq()
+  o.seq <- get_oseq(ride.seq)
+  ride(ride.seq, o.seq)
 }
 
 

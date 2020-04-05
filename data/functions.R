5ae860fbd1f9521ba23ae7b336e802df63a9f98d
Sean <maden@ohsu.edu>
2020-04-05 12:42:27 -0700

    idle alabel

--- data/functions.R
+++ data/functions.R
@@ -94,15 +94,15 @@ get_task_outcome <- function(task.prob, bcond, dmssg = ""){
 #' @param ssint Sleep interval for frames.
 #' @return NULL
 idle_ani <- function(fv.idle, logo, 
-                     alabel = "idle mode", 
+                     alabel = "\nidle mode", 
                      loops = 3, sleepint = si.idle){
-  plot.new()
+  grid.newpage()
   grid.raster(logo, width = 0.35, height = 0.25, 
               hjust = -0.2, vjust = 1.7)
   for(l in 1:loops){
-    grid.text(fv.idle[1], gp = gpar(col = "black"))
+    grid.text(c(fv.idle[1], alabel), gp = gpar(col = "black"))
     Sys.sleep(sleepint)
-    grid.text(fv.idle[1], gp = gpar(col = "white"))
+    grid.text(c(fv.idle[1], alabel), gp = gpar(col = "white"))
   }
   grid.newpage()
   return(NULL)
